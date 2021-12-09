74e37138df32754dc11de7829a923bddc6c1982d
Snowkedd <maxim.thomas.be@gmail.com>
2021-12-09 20:21:55 +0100

    Add - Init categories + GET all

---
+++ Backend/Controllers/CategoriesController.cs
@@ -0,0 +1,31 @@
+﻿using Backend.Logic;
+using Backend.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Backend.Controllers
+{
+    [Authorize]
+    [ApiController]
+    [Route("[controller]")]
+    public class CategoriesController : ControllerBase
+    {
+        private ICategorieLogic _CategorieLogic;
+
+        public CategoriesController(ICategorieLogic CategorieLogic)
+        {
+            _CategorieLogic = CategorieLogic;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<Categorie>>> GetAllCategories()
+        {
+            List<Categorie> categories = await _CategorieLogic.GetAllCategories();
+            return Ok(categories);
+        }
+    }
+}
---
+++ Backend/Logic/CategorieLogic.cs
@@ -0,0 +1,27 @@
+﻿using Backend.Models;
+using Backend.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Backend.Logic
+{
+    public interface ICategorieLogic
+    {
+        Task<List<Categorie>> GetAllCategories();
+    }
+    public class CategorieLogic : ICategorieLogic
+    {
+        private ICategorieService _CategorieServices;
+        public CategorieLogic(ICategorieService CategorieServices)
+        {
+            _CategorieServices = CategorieServices;
+        }
+
+        public async Task<List<Categorie>> GetAllCategories()
+        {
+            return await _CategorieServices.GetAllCategories();
+        }
+    }
+}
