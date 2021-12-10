7fba830de09e51e706ca2e6d98ef4fbf52b08951
Soulaimane <soulaimane@levelapp.be>
2021-12-10 17:42:00 +0100

    Add - Test

---
+++ Backend.Tests/MembersControllerTests.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace Backend.Tests
+{
+    public class MembersControllerTests
+    {
+        [Fact]
+        public void GetMember()
+        {
+            Assert.True(false);
+        }
+    }
+}
