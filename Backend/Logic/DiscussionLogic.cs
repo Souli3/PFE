23d5aaa9bfa9041412d19eea4c168c56b09026c0
Snowkedd <maxim.thomas.be@gmail.com>
2021-12-13 14:50:02 +0100

    Add - GET & POST messages

---
+++ Backend/Logic/DiscussionLogic.cs
@@ -0,0 +1,48 @@
+﻿using Backend.Models;
+using Backend.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Backend.Logic
+{
+    public interface IDiscussionLogic
+    {
+        Task<List<Message>> GetAllMessagesFromIdDiscussion(int id);
+        Membre VerifyUserFromToken(int id, string email);
+        Task<Message> PostMessageToDiscussion(Message message, Membre valide);
+    }
+    public class DiscussionLogic : IDiscussionLogic
+    {
+        private IDiscussionService _DiscussionService;
+        private IMembreServices _MembreServices;
+        public DiscussionLogic(IDiscussionService DiscussionService, IMembreServices MembreServices)
+        {
+            _DiscussionService = DiscussionService;
+            _MembreServices = MembreServices;
+        }
+
+        public async Task<List<Message>> GetAllMessagesFromIdDiscussion(int id)
+        {
+            return await _DiscussionService.GetAllMessagesFromIdDiscussion(id);
+        }
+
+        public async Task<Message> PostMessageToDiscussion(Message message, Membre valide)
+        {
+            message.Envoyeur_id = valide.Id;
+            message.Date_envoi = DateTime.Now;
+            return await _DiscussionService.PostMessageToDiscussion(message);
+        }
+
+        public Membre VerifyUserFromToken(int id, string email)
+        {
+            Discussion discussion = _DiscussionService.GetDiscussion(id);
+            Membre test1 = _MembreServices.GetMembre(discussion.Membre1_id);
+            Membre test2 = _MembreServices.GetMembre(discussion.Membre2_id);
+            if (test1.Email == email) return test1;
+            else if (test2.Email == email) return test2;
+            else return null;
+        }
+    }
+}
---
+++ Backend/Services/DiscussionService.cs
@@ -0,0 +1,40 @@
+﻿using Backend.Data;
+using Backend.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Backend.Services
+{
+    public interface IDiscussionService
+    {
+        Task<List<Message>> GetAllMessagesFromIdDiscussion(int id);
+        Discussion GetDiscussion(int id);
+        Task<Message> PostMessageToDiscussion(Message message);
+    }
+    public class DiscussionService : IDiscussionService
+    {
+        private IDataContext _dataContext;
+        public DiscussionService(IDataContext dataContext)
+        {
+            _dataContext = dataContext;
+        }
+        public async Task<List<Message>> GetAllMessagesFromIdDiscussion(int id)
+        {
+            return await _dataContext.Messages.Where(message => message.Discussion_id == id).ToListAsync();
+        }
+
+        public Discussion GetDiscussion(int id)
+        {
+            return _dataContext.Discussions.Where(discussion => discussion.Id == id).FirstOrDefault();
+        }
+
+        public async Task<Message> PostMessageToDiscussion(Message message)
+        {
+            Message messageDB = _dataContext.Messages.Add(message).Entity;
+            await _dataContext.SaveChangesAsync();
+            return messageDB;
+        }
+    }
+}
