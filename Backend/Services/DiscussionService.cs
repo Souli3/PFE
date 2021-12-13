5b150f65cc4971955185feca28ba97f87a9c5b40
Snowkedd <maxim.thomas.be@gmail.com>
2021-12-13 18:56:11 +0100

    Add - Get & Post discussions

--- Backend/Services/DiscussionService.cs
+++ Backend/Services/DiscussionService.cs
@@ -12,6 +12,9 @@ namespace Backend.Services
         Task<List<Message>> GetAllMessagesFromIdDiscussion(int id);
         Discussion GetDiscussion(int id);
         Task<Message> PostMessageToDiscussion(Message message);
+        Task<List<Discussion>> GetDiscussionsFromIdMembre(int id);
+        Discussion GetDiscussionBetween2Members(int id1, int id2);
+        Task<Discussion> CreateDiscussion(int id1, int id2);
     }
     public class DiscussionService : IDiscussionService
     {
@@ -20,6 +23,17 @@ namespace Backend.Services
         {
             _dataContext = dataContext;
         }
+
+        public async Task<Discussion> CreateDiscussion(int id1, int id2)
+        {
+            Discussion newDiscussion = new Discussion();
+            newDiscussion.Membre1_id = id1;
+            newDiscussion.Membre2_id = id2;
+            Discussion discussionDB = _dataContext.Discussions.Add(newDiscussion).Entity;
+            await _dataContext.SaveChangesAsync();
+            return discussionDB;
+        }
+
         public async Task<List<Message>> GetAllMessagesFromIdDiscussion(int id)
         {
             return await _dataContext.Messages.Where(message => message.Discussion_id == id).ToListAsync();
@@ -28,6 +42,23 @@ namespace Backend.Services
         public Discussion GetDiscussion(int id)
         {
             return _dataContext.Discussions.Where(discussion => discussion.Id == id).FirstOrDefault();
+        }
+
+        public Discussion GetDiscussionBetween2Members(int id1, int id2)
+        {
+            Discussion discussionDB;
+            discussionDB = _dataContext.Discussions.Where(discussion => discussion.Membre1_id == id1 && discussion.Membre2_id == id2).FirstOrDefault();
+            if (discussionDB != null) return discussionDB;
+            discussionDB = _dataContext.Discussions.Where(discussion => discussion.Membre1_id == id2 && discussion.Membre2_id == id1).FirstOrDefault();
+            if (discussionDB != null) return discussionDB;
+            return null;
+        }
+
+        public async Task<List<Discussion>> GetDiscussionsFromIdMembre(int id)
+        {
+            List<Discussion> list1 = await _dataContext.Discussions.Where(discussion => discussion.Membre1_id == id).ToListAsync();
+            List<Discussion> list2 = await _dataContext.Discussions.Where(discussion => discussion.Membre2_id == id).ToListAsync();
+            return list1.Union(list2).OrderBy(discussion => discussion.Id).ToList();
         }
 
         public async Task<Message> PostMessageToDiscussion(Message message)
